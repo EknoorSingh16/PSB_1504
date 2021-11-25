cee0485a2db02e53a6b006aceedd7db25e41e02e
Eknoor Singh <93900696+EknoorSingh16@users.noreply.github.com>
2021-11-25 15:21:07 +0530

    Create Time.c

---
+++ Time.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	//speed=distance/time
+	//time=distance/speed
+	
+	int S,D,T;
+	S = 10;
+	D = 20;
+	T = D/S;
+	
+	printf("The Total Distance is: %d",&T);
+	
+	return 0;
+}
