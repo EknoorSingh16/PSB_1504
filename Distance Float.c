2a6ec151dbf2a5468789d54690f7fe0310cd4447
Eknoor Singh <93900696+EknoorSingh16@users.noreply.github.com>
2021-11-25 19:43:30 +0530

    Create Distance Float.c

---
+++ Distance Float.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	//speed=distance/time
+	//distance=speed/time
+	
+	float D, S, T;
+	S = 20;
+	T = 2;
+	D = S/T;
+	
+	printf("The Total distance is : %.2f",D);
+	
+	return 0;
+	
+}
