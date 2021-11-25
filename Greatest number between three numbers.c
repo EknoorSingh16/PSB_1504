a18d2c8b1eb0ee335d07c69a1ca59e68397d3b7f
Eknoor Singh <93900696+EknoorSingh16@users.noreply.github.com>
2021-11-25 19:41:19 +0530

    Create Greatest number between three numbers.c

---
+++ Greatest number between three numbers.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+	int num1, num2, num3;
+	
+	printf("Enter first number: ");
+	scanf("%d",&num1);
+	
+	printf("Enter second number: ");
+	scanf("%d",&num2);
+	
+	printf("Enter third number: ");
+	scanf("%d",&num3);
+	
+	//if num1 is greater number
+	if (num1>=num2  && num1>=num3)
+	printf("The greatest number is: %d",num1);
+	
+	//if num2 is greater number
+	if (num2>=num1 && num2>=num3)
+	{
+	printf("\nThe greatest number is: %d",num2);
+	}
+	//if num3 is greater number
+	else
+	{
+	printf("\nThe greatest number is: %d",num3);
+	}
+	return 0;
+	
+}
