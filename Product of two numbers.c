12f59cca8d97c6d468ce1dd517123514e7b343ff
Eknoor Singh <93900696+EknoorSingh16@users.noreply.github.com>
2021-11-25 15:28:45 +0530

    Create Product of two numbers.c

---
+++ Product of two numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int num1, num2, product;
+	printf("enter first number");
+	scanf("%d",&num1);
+	printf("enter second number");
+	scanf("%d",&num2);
+	product=num1 * num2;
+	printf("%d",&product);
+	return 0;
+}
