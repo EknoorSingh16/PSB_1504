efbf3454ff11471caf7ec7c5b43d63f2d8d69ab3
Eknoor Singh <93900696+EknoorSingh16@users.noreply.github.com>
2021-11-25 15:21:40 +0530

    Create Time in mins.c

---
+++ Time in mins.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	//1hour = 60 min
+	//1 min = 60 sec
+	
+	int Hour=3;
+	int min=Hour*60;
+	
+	printf("The Time in mins is: %d",min);
+	
+	return 0;
+}
