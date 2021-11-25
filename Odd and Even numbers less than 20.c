6b3ffc0f419df66fe854d5076e82152794f09403
Eknoor Singh <93900696+EknoorSingh16@users.noreply.github.com>
2021-11-25 15:35:54 +0530

    Create Odd and Even numbers less than 20.c

---
+++ Odd and Even numbers less than 20.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int a=0;
+	
+	for(a=0;a<=20;a++)
+	if(a%2==0)
+	{
+	
+	printf("\nThe even numbers are: %d",a);
+}
+	for(a=1;a<20;a++)
+	if(a%2==1)
+	{
+		printf("\nThe odd numbers are: %d",a);
+	}
+}
