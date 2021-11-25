8cdbea58e9e1770ca491cc6dd67d1bd604ad4f74
Eknoor Singh <93900696+EknoorSingh16@users.noreply.github.com>
2021-11-25 15:28:10 +0530

    Create Remainder without modulus.c

---
+++ Remainder without modulus.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	int no, divisor, remainder;
+	
+	printf("Enter the number: ");
+	scanf("%d",&no);
+	
+	printf("Enter the divisor: ");
+	scanf("%d",&divisor);
+	
+	while (no>=divisor) {
+		no = no - divisor;
+	}
+	
+	remainder = no;
+	
+	printf("The remainder is: %d",remainder);
+	
+	return 0;
+}
