d69f72258abe71c76f7c8c0bc3819f77f7ed3241
Eknoor Singh <93900696+EknoorSingh16@users.noreply.github.com>
2021-11-25 19:53:32 +0530

    Create Addition of two numbers.c

---
+++ Addition of two numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int num1, num2, add;
+	
+	printf("Enter first number: ");
+	scanf("%d",&num1);
+	
+	printf("Enter second number: ");
+	scanf("%d",&num2);
+	
+	add = num1 + num2;
+	
+	printf("The addition of the numbers is: %d",add);
+	
+	return 0;
+}
