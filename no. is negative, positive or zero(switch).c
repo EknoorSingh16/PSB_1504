2b82c88e82710dc24898354e1ebd4682b1d71670
Eknoor Singh <93900696+EknoorSingh16@users.noreply.github.com>
2021-12-02 15:41:21 +0530

    Create no. is negative, positive or zero(switch).c

---
+++ no. is negative, positive or zero(switch).c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+	int n;
+	
+	printf("Enter any number: ");
+	scanf("%d",&n);
+	
+	switch(n>0)
+	{
+		case 1:
+			printf("The chosen number:%d is positive",n);
+			break;
+			
+		case 0:
+			switch(n<0)
+			{
+				case 1:
+					printf("The chosen number:%d is negative",n);
+					break;
+				case 0:
+					printf("The chosen number is zero");
+					break;
+			}
+	}
+	return 0;
+}
