9d541e661fbbccaffa7bbe0a4a582b4f6ce764b4
Eknoor Singh <93900696+EknoorSingh16@users.noreply.github.com>
2021-12-02 15:33:16 +0530

    Create minimum among three numbers (if else).c

---
+++ minimum among three numbers (if else).c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+	int n1,n2,n3;
+	
+	printf("Enter first number: ");
+	scanf("%d",&n1);
+	
+	printf("Enter second number: ");
+	scanf("%d",&n2);
+	
+	printf("Enter third number: ");
+	scanf("%d",&n3);
+	
+	if(n1<n2 && n1<n3)
+	{
+		printf("The minimum number is: %d",n1);
+	}
+	else if(n2<n1 && n2<n3)
+	{
+		printf("The minimum number is: %d",n2);
+	}
+	else
+	{
+		printf("The minimum number is: %d",n3);
+	}
+	return 0;
+}
