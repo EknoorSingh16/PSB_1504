16dc9a442f84cb518001eaae0ae2a0d785608879
Eknoor Singh <93900696+EknoorSingh16@users.noreply.github.com>
2022-01-11 19:07:04 +0530

    Create LSB of a number.c

---
+++ LSB of a number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int n;
+	printf("Enter any number: ");
+	scanf("%d",&n);
+	
+	if(n&1)
+	{
+		printf("LSB of %d is set(1)",n);
+	}
+	else
+	{
+		printf("LSB of %d is set(0)",n);
+	}
+	
+	return 0;
+}
